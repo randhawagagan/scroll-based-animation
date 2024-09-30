f4baa80e79e123b5c28172ea5a4992186f1b07ff
Gagan Randhawa <gaganr@addatech.ca>
2024-09-30 17:10:40 -0400

    sample portfolio with parallex particles and animatimn

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
-import GUI from 'lil-gui'
+import GUI from 'lil-gui';
+import gsap from 'gsap';
 
 /**
  * Debug
@@ -13,7 +14,8 @@ const parameters = {
 gui
     .addColor(parameters, 'materialColor')
     .onChange(() => {
-        material.color.set(parameters.materialColor)
+        material.color.set(parameters.materialColor);
+        particlesMaterial.color.set(parameters.materialColor)
     })
 
 /**
@@ -60,9 +62,36 @@ mesh1.position.y = - objectsDistance * 0;
 mesh2.position.y = - objectsDistance * 1;
 mesh3.position.y = - objectsDistance * 2;
 
+mesh1.position.x =  2;
+mesh2.position.x = -2;
+mesh3.position.x = 2;
+
 scene.add(mesh1, mesh2, mesh3);
 
 const sectionMeshes = [mesh1, mesh2, mesh3]
+
+const particlesCount = 200; 
+
+const positions = new Float32Array(particlesCount * 3);
+
+
+
+for(let i = 0; i< particlesCount; i++){
+    positions[i * 3 + 0] = (Math.random() -  0.5) * 10  
+    positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length
+    positions[i * 3 + 2] = (Math.random() -  0.5) * 10  
+}
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+
+const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    size: 0.03,
+    sizeAttenuation: true
+});
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles)
 
 const directionalLight = new THREE.DirectionalLight('#ffffff', 3);
 
@@ -92,13 +121,16 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup)
 /**
  * Camera
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
@@ -112,25 +144,57 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 
 let scrollY = window.scrollY;
+let currentSection = 0;
 
 window.addEventListener('scroll', ()=>{
-    scrollY = window.scrollY
+    scrollY = window.scrollY;
+    const newSection = scrollY / sizes.height;
+  if(newSection != currentSection){
+    currentSection = newSection;
+    gsap.to(
+        sectionMeshes[currentSection].rotation,
+        {
+            duration : 1.5, 
+            ease: 'power2.inOut',
+            x: '+=6',
+             y: '+=3'
+        })
+  }
+    
+})
+
+const cursor = {};
+cursor.x = 0; 
+cursor.y = 0;
+
+window.addEventListener('mousemove', (event)=>{
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = event.clientY / sizes.height - 0.5;
+
 })
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
+let previousTime = 0;
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
+    const elapsedTime = clock.getElapsedTime();
+    const deltaTime = elapsedTime - previousTime;
+    previousTime = elapsedTime
 
-    camera.position.y = - scrollY / sizes.height * objectsDistance
+    camera.position.y = - scrollY / sizes.height * objectsDistance;
+
+    const parallaxX = cursor.x * 0.5;
+    const parallaxY = - cursor.y * 0.5;
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x)  * 5 * deltaTime
+    cameraGroup.position.y += (parallaxY- cameraGroup.position.y) * 5 * deltaTime
 
     for(const mesh of sectionMeshes){
-        mesh.rotation.x = elapsedTime * 0.1;
-        mesh.rotation.y = elapsedTime * 0.12;
+        mesh.rotation.x += deltaTime * 0.1;
+        mesh.rotation.y += deltaTime * 0.12;
     }
 
     // Render
